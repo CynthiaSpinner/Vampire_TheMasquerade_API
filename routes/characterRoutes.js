a10a0e661e6ceb1cf3c1b6787baedca2f3698fa0
Cynthia Spinner <CynthiaSpinner@gmail.com>
2025-12-04 16:42:55 -0600

    fix change post to status 201 return character for success path

--- routes/characterRoutes.js
+++ routes/characterRoutes.js
@@ -24,7 +24,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
     try {
         const character = await characterQueries.createCharacter(req.body);
-        res.status(500).json({ error: error.message });
+        res.status(201).json(character);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
