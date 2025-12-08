0b87ab1d1dbac28ec0d7ac29971f86e70c8efbce
Cynthia Spinner <CynthiaSpinner@gmail.com>
2025-12-08 12:22:57 -0600

    made navBar

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,42 @@
+import { Link, useLocation } from 'react-router-dom';
+import './NavBar.css';
+
+function NabBar() {
+    const location = useLocation();
+
+    return (
+        <nav className="navbar">
+            <div className="navbar-brand">
+                <Link to="/">VtM GM Hub</Link>
+            </div>
+            <ul className="navbar-nav">
+                <li>
+                    <Link 
+                        to="/" 
+                        className={location.pathname === '/' ? 'active' : ''}
+                    >
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link 
+                        to="/characters" 
+                        className={location.pathname.startsWith('/characters') ? 'active' : ''}
+                    >
+                        Characters
+                    </Link>
+                </li>
+                <li>
+                    <Link 
+                        to="/stories" 
+                        className={location.pathname === '/stories' ? 'active' : ''}
+                    >
+                        Stories
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
