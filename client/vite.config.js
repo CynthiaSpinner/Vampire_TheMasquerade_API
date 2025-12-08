dcfc2fa91cd6ab8a6c529dc51a2302237331ce17
Cynthia Spinner <CynthiaSpinner@gmail.com>
2025-12-08 11:39:19 -0600

    added comments

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,14 @@
+// vite config for react frontend & proxying api requests to express server
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
     server: {
-        port: 3001,
+        port: 3001, // dev frontend runs here
         proxy: {
             '/api': {
-                target: 'http://localhost:3000',
+                target: 'http://localhost:3000', // backend api server
                 changeOrigin: true
             }
         }
